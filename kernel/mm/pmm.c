2e84ed62088bc3a972e389d7d54a49bfc62205f9
Hooman <68755769+HoomanBud@users.noreply.github.com>
2023-06-12 17:11:38 +0300

    PMM

--- kernel/mm/pmm.c
+++ kernel/mm/pmm.c
@@ -10,42 +10,41 @@ typedef struct FMemRegion {
 
 FMemRegion* freelist = NULL;
 
-void FreelistPMMInit(struct limine_memmap_entry *mem_map, size_t mem_map_entries) {
+#define PAGE_SIZE 4096
+
+void FreelistPMMInit(struct limine_memmap_entry* mem_map, size_t mem_map_entries) {
     for (size_t i = 0; i < mem_map_entries; i++) {
         struct limine_memmap_entry* entry = &mem_map[i];
-        if (entry->type == 0) {
-            FMemRegion* region = (FMemRegion*)entry->base;
-            region->start = entry->base;
-            region->size = entry->length;
-            region->next = freelist;
-            freelist = region;
+        if (entry->type == 1) {
+            uint64_t start_page = (entry->base + PAGE_SIZE - 1) / PAGE_SIZE;
+            uint64_t end_page = entry->base + entry->length;
+            end_page /= PAGE_SIZE;
+
+            for (uint64_t page = start_page; page < end_page; page++) {
+                FMemRegion* region = (FMemRegion*)(page * PAGE_SIZE);
+                region->start = page * PAGE_SIZE;
+                region->size = PAGE_SIZE;
+                region->next = freelist;
+                freelist = region;
+            }
         }
     }
 }
 
-void* allocate(size_t size) {
-    FMemRegion* prev = NULL;
-    FMemRegion* current = freelist;
-    while (current != NULL) {
-        if (current->size >= size) {
-            if (prev == NULL) {
-                freelist = current->next;
-            } else {
-                prev->next = current->next;
-            }
-            return (void*)current->start;
-        }
-        prev = current;
-        current = current->next;
+void* allocate_page() {
+    if (freelist != NULL) {
+        FMemRegion* allocated_page = freelist;
+        freelist = freelist->next;
+        return (void*)allocated_page->start;
     }
 
     return NULL;
 }
 
-void deallocate(void* ptr, size_t size) {
-    FMemRegion* region = (FMemRegion*)ptr;
-    region->start = (uint64_t)ptr;
-    region->size = size;
+void deallocate_page(void* page) {
+    FMemRegion* region = (FMemRegion*)page;
+    region->start = (uint64_t)page;
+    region->size = PAGE_SIZE;
     region->next = freelist;
     freelist = region;
 }
